56da09123c3ed292a5ce904a33dc1e86b081d94e
Himanshu2326 <himanshuprajapati71305@gmail.com>
2024-07-03 00:30:06 +0530

    SkyCast

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+// Load environment variables from .env file
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+      __VITE_WEATHER_API_KEY__: JSON.stringify(process.env.VITE_WEATHER_API_KEY),
+  },
 })
